23f6c08269f6bcef8b15c98647b2dc0454825816
Larbi-Fer <ferhaoui.20044@gmail.com>
2025-02-09 09:55:34 +0100

    ADD Button componant

---
+++ client/ui/Button/index.tsx
@@ -0,0 +1,53 @@
+'use client'
+import { useRef, useState } from 'react'
+import './style.css'
+
+type ButtonProps = {
+  children: React.ReactNode,
+  onClick?: () => void,
+  background?: string,
+  fullWidth?: boolean,
+  transparent?: boolean
+} & React.ButtonHTMLAttributes<HTMLButtonElement>
+
+const Button = ({ children, transparent, onClick, background, fullWidth, ...props } : ButtonProps) => {
+  const btnRef = useRef<HTMLButtonElement>(null)
+  const [spans, setSpans] = useState<React.ReactNode[]>([])
+
+  const handelClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    onClick && onClick()
+
+    const mouseX = e.clientX;
+    const mouseY = e.clientY;
+
+
+    const buttonRect = btnRef.current?.getBoundingClientRect();
+    if(!buttonRect) return;
+    const buttonX = buttonRect.left;
+    const buttonY = buttonRect.top;
+
+    // Calculate the mouse position relative to the button
+    const mouseRelativeX = mouseX - buttonX;
+    const mouseRelativeY = mouseY - buttonY;
+
+    // setSpans( old => [...old, <span key={Math.random()} style={{top: (e.pageY - btnRef.current?.offsetTop!) + "px", left: (e.pageX - btnRef.current?.offsetLeft!) + "px"}} />] )
+    setSpans( old => [...old, <span key={Math.random()} style={{top: (mouseRelativeY) + "px", left: (mouseRelativeX) + "px"}} />] )
+    setTimeout(() => {
+      setSpans( old => old.slice(1) )
+    }, 1000);
+  }
+
+  const btnStyle: React.CSSProperties = background ? {background} : {}
+  if (fullWidth) btnStyle.width = '100%'
+
+  return (
+    <div className="button-container">
+      <button {...props} className={'button' + ( transparent ? ' transparent' : ' primary' )} onClick={handelClick} ref={btnRef} style={btnStyle}>
+        <p>{children}</p>
+        { spans.map(span => span) }
+      </button>
+    </div>
+  )
+}
+
+export default Button
