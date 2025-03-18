a67e1d840334db55fffcac9e7df54493f10bb69b
Larbi-Fer <ferhaoui.20044@gmail.com>
2025-03-18 01:41:55 +0100

    ADD animation

--- client/components/dashboard/index.tsx
+++ client/components/dashboard/index.tsx
@@ -4,32 +4,48 @@ import WhatNaxt from "./LeftSide/WhatNaxt"
 import RecomChallenges from "./LeftSide/RecomChallenges"
 import LevelCard from "./RightSide/LevelCard"
 import StreakCalendar from "./RightSide/StreakCalendar"
+import * as motion from 'motion/react-client'
 
 import './style.css'
+
+const animation = {
+  hidden: { y: 5, opacity: 0 },
+  show: { y: 0, opacity: 1, transition: { staggerChildren: 0.15 } },
+}
 
 const Dashboard = () => {
   return (
     <div className="dashboard-container">
-      <div className="left">
-        <Grid container spacing={4} className="cards">
-          <Grid size={6}>
-            <Card />
+      <motion.div className="left" variants={animation} initial='hidden' animate='show'>
+        <motion.div variants={animation}>
+          <Grid container spacing={4} className="cards">
+            <Grid size={6}>
+              <Card />
+            </Grid>
+            <Grid size={6}>
+              <Card />
+            </Grid>
           </Grid>
-          <Grid size={6}>
-            <Card />
-          </Grid>
-        </Grid>
+        </motion.div>
 
-        <WhatNaxt />
+        <motion.div variants={animation}>
+          <WhatNaxt />
+        </motion.div>
 
-        <RecomChallenges />
-      </div>
+        <motion.div variants={animation}>
+          <RecomChallenges />
+        </motion.div>
+      </motion.div>
 
-      <div className="right">
-        <LevelCard />
+      <motion.div className="right" variants={animation} initial='hidden' animate='show'>
+        <motion.div variants={animation}>
+          <LevelCard />
+        </motion.div>
 
-        <StreakCalendar />
-      </div>
+        <motion.div variants={animation}>
+          <StreakCalendar />
+        </motion.div>
+      </motion.div>
     </div>
   )
 }
