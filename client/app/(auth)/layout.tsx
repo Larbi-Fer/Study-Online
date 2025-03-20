68d3bb385ed57c128832ebd0ddda0509202863d5
Larbi-Fer <ferhaoui.20044@gmail.com>
2025-03-20 06:55:17 +0100

    Add user authentication redirection in layout components

--- client/app/(auth)/layout.tsx
+++ client/app/(auth)/layout.tsx
@@ -1,4 +1,6 @@
 import AuthForm from '@/components/Auth/AuthForm';
+import { cookies, headers } from 'next/headers';
+import { redirect } from 'next/navigation';
 import React from 'react'
 
 const AuthLayout = async ({
@@ -6,6 +8,8 @@ const AuthLayout = async ({
 }: Readonly<{
   children: React.ReactNode;
 }>) => {
+  const userId = (await cookies()).get('userId');
+  if (userId) return redirect('/dashboard');
   return (
     <div>
       <AuthForm />
--- client/app/(root)/layout.tsx
+++ client/app/(root)/layout.tsx
@@ -1,16 +1,20 @@
 import type { Metadata } from "next";
 import Navbar from "@/components/Navbar";
 import CodingSapce from "@/components/CodingSpace";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
 export const metadata: Metadata = {
   title: 'Dashboard'
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const userId = (await cookies()).get('userId');
+  if (!userId) return redirect('/login');
   return (
     <>
       <Navbar type="user" />
