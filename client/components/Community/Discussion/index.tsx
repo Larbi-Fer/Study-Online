cec46e870cfa3a5ed230a7de772fdc82cbddb82e
Larbi-Fer <ferhaoui.20044@gmail.com>
2025-04-23 09:58:13 +0100

    CLEAN

--- client/components/Community/Discussion/index.tsx
+++ client/components/Community/Discussion/index.tsx
@@ -6,7 +6,7 @@ import './style.css'
 const Discussion = ({discussion}: {discussion: DiscussionDetailsArgs}) => {
   return (
     <div className="discussion-details">
-      <h3>Discussion</h3>
+      <h3>{discussion.title}</h3>
       <Details discussion={discussion} />
       <h3>Comments</h3>
       <Comments comments={discussion.comments} />
